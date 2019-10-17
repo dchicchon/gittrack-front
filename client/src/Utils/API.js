2ecbc2e4871a1520ec282d5f0395df218a68bb60
dchicchon <danielchicchon@gmail.com>
2019-10-17 11:36:52 -0700

    change util API to use backend api

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
-
+let url = 'https://sheltered-shore-07884.herokuapp.com'
 export default {
 
     // General
     // ============
     editAccount: (creds) => {
-        return axios.put("/api/settings", creds)
+        return axios.put(`${url}/api/settings`, creds)
     },
 
     // Auth functions
     // =================
     getUser: () => {
-        return axios.get("/auth/user")
+        return axios.get(`${url}/auth/user`)
     },
 
     // Handle User Login
     handleLogin: (creds) => {
-        return axios.post("/auth/login", creds)
+        return axios.post(`${url}/auth/login`, creds)
     },
 
     logout: function () {
-        return axios.get("/auth/logout")
+        return axios.get(`${url}/auth/logout`)
     },
 
     handleSignup: (creds) => {
-        return axios.post("/auth/signup", creds)
+        return axios.post(`${url}/auth/signup`, creds)
     },
 
 
@@ -34,62 +34,62 @@ export default {
     // =======================
 
     createAccount: (creds) => {
-        return axios.post("/auth/signup", creds)
+        return axios.post(`${url}/auth/signup`, creds)
     },
 
     adminList: () => {
-        return axios.get("/api/admin/")
+        return axios.get(`${url}/api/admin/`)
     },
 
     instructorList: () => {
-        return axios.get("/api/admin/instructor")
+        return axios.get(`${url}/api/admin/instructor`)
     },
 
     studentList: () => {
-        return axios.get("/api/admin/student")
+        return axios.get(`${url}/api/admin/student`)
     },
 
     deleteAdmin: (id) => {
-        return axios.delete("/api/admin/" + id)
+        return axios.delete(`${url}/api/admin/` + id)
     },
 
     deleteInstructor: (id) => {
-        return axios.delete("/api/admin/instructor/" + id)
+        return axios.delete(`${url}/api/admin/instructor/` + id)
     },
 
     deleteStudent: (id) => {
-        return axios.delete("/api/admin/student/" + id)
+        return axios.delete(`${url}/api/admin/student/` + id)
     },
 
     // ======================
     // Instructor Functions
     // =======================
     getCohorts: id => {
-        return axios.get("/api/instructor/" + id)
+        return axios.get(`${url}/api/instructor/` + id)
     },
 
     cohortStudentList: id => {
-        return axios.get("/api/instructor/cohorts/" + id)
+        return axios.get(`${url}/api/instructor/cohorts/` + id)
     },
 
     cohortCreate: creds => {
-        return axios.post("/api/instructor/cohorts", creds)
+        return axios.post(`${url}/api/instructor/cohorts`, creds)
     },
 
     studentCreate: creds => {
-        return axios.post("/api/instructor/students", creds)
+        return axios.post(`${url}/api/instructor/students`, creds)
     },
-    
+
     inspectStudent: id => {
-        return axios.get("/api/instructor/students/" + id)
+        return axios.get(`${url}/api/instructor/students/` + id)
     },
 
     studentRemove: id => {
-        return axios.delete("/api/instructor/students/" + id)
+        return axios.delete(`${url}/api/instructor/students/` + id)
     },
 
     getGraph: list => {
-        return axios.post("/api/instructor/cohorts/graph", list)
+        return axios.post(`${url}/api/instructor/cohorts/graph`, list)
     },
 
 
@@ -98,11 +98,11 @@ export default {
     // =====================
 
     editGithubUsername: (creds) => {
-        return axios.put("/api/student/ghUsername", creds)
+        return axios.put(`${url}/api/student/ghUsername`, creds)
     },
 
     getMyData: (username) => {
-        return axios.get("/api/student/" + username)
+        return axios.get(`${url}/api/student/` + username)
     }
 
 }
